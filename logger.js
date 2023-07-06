123b5cc465b5948ba15a894eefb70901141f102b
topias-huawei-wsl <topias.tauriainen@gmail.com>
2023-07-06 13:14:11 +0300

    Fix more errrors

--- logger.js
+++ logger.js
@@ -1,16 +1,17 @@
 import { serve } from 'https://deno.land/std@0.171.0/http/server.ts'
 
 const handleRequest = async (request) => {
-  const { method, url, headers } = request;
-  const body = new Uint8Array(await Deno.readAll(request.body));
+  const { method, url, headers, body } = request;
+
+
 
   console.log('=== New Request ===');
   console.log(`Method: ${method}`);
   console.log(`URL: ${url}`);
   console.log('Headers:', headers);
-  console.log('Body:', new TextDecoder().decode(body));
+  console.log('Body:', await request.text());
 
-  request.respond({ body: 'Request logged successfully\n' });
+  return new Response('Request logged successfully\n');
 }
 
 serve(handleRequest, { port: 3000 })
